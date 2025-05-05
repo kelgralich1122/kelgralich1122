edf570ffd90de43269edf057f6951c0fccde15e0
kelgralich1122 <kelvingrayson1122@gmail.com>
2025-05-05 23:54:49 +0300

    Delete script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Like Button Functionality
-document.querySelectorAll('.like-btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-        alert('You liked this content!');
-    });
-});
 
-// Comment Button Functionality
-document.querySelectorAll('.comment-btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-        alert('Feature coming soon!');
-    });
-});
